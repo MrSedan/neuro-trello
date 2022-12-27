99209b65a6934d5c72ede063fe0a3c505fe06031
MrSedan <elpashevs@bk.ru>
2022-12-27 15:19:58 +0300

    Initial

---
+++ backend/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+
+const app: express.Application = express();
+const upload: multer.Multer = multer({dest: 'uploads/'});
+
+const port: number = 3500;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', (_req, _res) => {
+    _res.status(200).send({"status": "ok"});
+});
+
+app.post('/aboba', (_req, _res) => {
+    _res.status(200).json(_req.body);
+});
+
+app.post('/profile', upload.single('avatar'), (_req, _res, _next) => {
+    console.log(_req.file, _req.body);
+    _res.json(_req.body);
+});
+
+app.listen(port, () => {
+    console.log(`TypeScript with Express on http://localhost:${port}/`);
+});
