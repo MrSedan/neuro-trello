a167836b30f34c95107f919c4e7783a52c25c58f
MrSedan <elpashevs@bk.ru>
2022-12-31 00:29:32 +0300

    Some changes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,13 +7,16 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
+function tick(){
+  root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
+  }
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
