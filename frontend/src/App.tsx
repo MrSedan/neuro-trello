1030999f3bedbf291963e5acb3a484531232a42a
MrSedan <elpashevs@bk.ru>
2022-12-31 00:29:19 +0300

    Axios added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface NameProps { 
+  name?: string;
 }
 
+interface NameState {
+  date: Date,
+  username: string
+}
+
+class App extends React.Component<NameProps, NameState> {
+  timerID?: ReturnType<typeof setInterval>;
+  constructor(props: NameProps){
+    super(props);
+    this.state = {date: new Date(), username: ""}
+    this.getUser = this.getUser.bind(this);
+  }
+
+  componentDidMount(): void {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+  }
+
+  tick(): void {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  componentWillUnmount(): void {
+      clearInterval(this.timerID);
+  }
+
+  render(): React.ReactNode {
+      return (
+        <div>
+          <h1>Your username is {this.state.username}</h1>
+          <button onClick={this.getUser}>Test</button>
+        </div>
+      )
+  }
+  getUser(): void {
+    axios.get('http://localhost:3500/user/get/2').then(response => {
+      console.log(response.data.username);
+      const username: string = response.data.username;
+      this.setState({
+        username: username
+      });
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }
+}
 export default App;
