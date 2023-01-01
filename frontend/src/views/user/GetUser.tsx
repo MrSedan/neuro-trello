cf94e0545b8b747983a7453bebc47571db68ef74
MrSedan <elpashevs@bk.ru>
2023-01-01 23:28:05 +0300

    Get user page from backend

--- frontend/src/views/user/GetUser.tsx
+++ frontend/src/views/user/GetUser.tsx
@@ -1,19 +1,51 @@
+import axios from "axios";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { withRouter } from "../../tools/withRouter";
 
-class GetUserPage extends React.Component<{params: {id: string}}> {
+interface GetUserState {
+    username?: string
+}
+
+class GetUserPage extends React.Component<{params: {id: string}}, GetUserState> {
     user_id?: string;
+
     constructor(props: {params: {id: string}}){
         super(props);
         const { id } = this.props.params;
         this.user_id = id;
-        
+        this.state = {
+            username: ''
+        }
     }
+
+    componentDidMount(): void {
+        this.getUser();
+    }
+
+    async getUser() {
+        console.log('Aboba1');
+        await axios.get(`http://localhost:3500/user/get/${this.user_id}`)
+            .then((response)=>{
+                console.log('Aboba2');
+                const data = response.data;
+                console.log(data);
+                this.setState({
+                    username: data.username
+                });
+            }).catch(e => {
+                this.setState({
+                    username: e.toJSON().status
+                });
+            })
+    }
+    
 
     render() {
         return (
-            <h1>{this.user_id}</h1>
+            <div>
+                <h1>Username: {this.state.username}</h1>
+            </div>
         )
     }
 }
