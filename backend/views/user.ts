821c7042a09553031cb186435c36281beaffc4a4
MrSedan <elpashevs@bk.ru>
2023-01-01 23:27:10 +0300

    Pass hashing

--- backend/views/user.ts
+++ backend/views/user.ts
@@ -1,17 +1,20 @@
 import {Router} from 'express';
 import { PrismaClient } from '@prisma/client';
+import * as crypto from 'crypto';
 
 const router = Router();
 const prisma = new PrismaClient();
 
 router.put('/create', async (_req, _res) => {
     const { username, password } = _req.body
+    const salt = crypto.randomBytes(16).toString('hex');
+    const pass_hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
     try {
         const result = await prisma.user.create({
             data: {
                 username: username,
-                pass_hash: password,
-                salt: "qqq"
+                pass_hash: pass_hash,
+                salt: salt
             }
         })
         _res.status(201).json(result)
