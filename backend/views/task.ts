5a953b18c73448deb99b5a46022e71c099f6850e
MrSedan <elpashevs@bk.ru>
2023-05-20 00:34:16 +0300

    Tasks methods

---
+++ backend/views/task.ts
@@ -0,0 +1,53 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import { userMiddleware } from "./tools/middleware";
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.use(userMiddleware);
+
+router.get("/", async (_req, _res) => {
+    try {
+        const result = await prisma.task.findMany();
+        _res.status(200).json(result);
+    } catch (error) {
+        _res.status(500).json({ error: error });
+    }
+});
+
+router.post("/new", async (_req, _res) => {
+    const name = _req.body.name;
+    const category_id = _req.body.category_id;
+    try {
+        const result = await prisma.task.create({
+            data: {
+                category: { connect: { id: category_id } },
+                name: name,
+            },
+        });
+        _res.status(201).json(result);
+    } catch (error) {
+        _res.status(500).json({ error: error });
+    }
+});
+
+router.post("/move", async (_req, _res) => {
+    const id = _req.body.id;
+    const category_id = _req.body.category_id;
+    try {
+        const result = await prisma.task.update({
+            where: {
+                id: id,
+            },
+            data: {
+                category: { connect: { id: category_id } },
+            },
+        });
+        _res.status(200).json(result);
+    } catch (error) {
+        _res.status(500).json({ error: error });
+    }
+});
+
+export default router;
