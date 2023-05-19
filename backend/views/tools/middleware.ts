0012f867fcb7692cb0820eeb97c2627d733c4ae9
MrSedan <elpashevs@bk.ru>
2023-05-20 00:32:14 +0300

    Password check middleware

---
+++ backend/views/tools/middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+import { config as dotenv_config } from "dotenv";
+
+dotenv_config();
+dotenv_config({ path: ".env.local", override: true });
+
+export const userMiddleware: RequestHandler = (
+    _req: Request,
+    _res: Response,
+    next: NextFunction
+) => {
+    const authHandler = _req.headers.authorization;
+    if (!authHandler)
+        return _res.status(401).json({ error: "pass is missing" });
+    if (authHandler === process.env.PASSWORD) return next();
+    _res.status(401).json({ error: "wrong password" });
+};
