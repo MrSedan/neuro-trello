06b871d6ae4ac0826e7a1bbd362989b9d406dff0
MrSedan <elpashevs@bk.ru>
2023-05-20 00:32:00 +0300

    For user interactions in future...

--- backend/views/user.ts
+++ backend/views/user.ts
@@ -31,54 +31,76 @@ router.put('/create', async (_req, _res) => {
     }
 })
 
-router.get('/get/:id', async (_req, _res) => {
+//? For user create/login via username & password
+/*
+router.get("/get/:id", async (_req, _res) => {
     const { id } = _req.params;
     const result = await prisma.user.findUnique({
         where: {
-            id: Number(id)
+            id: Number(id),
         }, 
         select: {
             id: true,
-            username: true
-        }
-    })
-    if (result){
-        _res.status(200).json(result)
+            username: true,
+        },
+    });
+    if (result) {
+        _res.status(200).json(result);
     } else {
-        _res.status(404).json({error: "User not found"})
+        _res.status(404).json({ error: "User not found" });
     }
-})
-
-router.post('/login', async (_req, _res) => {
-    let username = '';
-    let password = '';
-    try{
+});
+router.put("/create", async (_req, _res) => {
+    let username = "";
+    let password = "";
+    try {
         username = _req.body.username;
         password = _req.body.password;
-    } catch (error){
-        _res.send(400).json({error: "bad request"})
-        return
+    } catch (error) {
+        _res.send(400).json({ error: "bad request" });
+        return;
+        }
+    const salt = crypto.randomBytes(16).toString("hex");
+    const pass_hash = crypto
+        .pbkdf2Sync(password, salt, 1000, 64, `sha512`)
+        .toString(`hex`);
+    try {
+        const result = await prisma.user.create({
+            data: {
+                username: username,
+                pass_hash: pass_hash,
+                salt: salt,
+            },
+        });
+        _res.status(201).json(result);
+    } catch (error) {
+        _res.status(400).json({ error: error });
+    }
+});
+router.post("/login", async (_req, _res) => {
+    let username = "";
+    let password = "";
+    try {
+        username = _req.body.username;
+        password = _req.body.password;
+    } catch (error) {
+        _res.send(400).json({ error: "bad request" });
+        return;
     }
     const user = await prisma.user.findUnique({
         where: {
-            username: username
-        }
-    })
-    if (user){
-        if (validPassword(password, user.pass_hash, user.salt)){
-            _res.status(200).json({status: "ok"});
+            username: username,
+        },
+    });
+    if (user) {
+        if (validPassword(password, user.pass_hash, user.salt)) {
+            _res.status(200).json({ status: "ok" });
         } else {
-            _res.status(401).json({error: "Invalid password or username"})
+            _res.status(401).json({ error: "Invalid password or username" });
         }
     } else {
-        _res.status(401).json({error: "Invalid password or username"})
+        _res.status(401).json({ error: "Invalid password or username" });
     }
-
-})
-
-function validPassword(password: string, pass_hash: string, salt: string): boolean {
-    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
-    return hash === pass_hash;
-}
+});*/
 
 export default router;
