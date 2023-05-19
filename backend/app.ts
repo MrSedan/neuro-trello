d079f8b5871a3a82a55b7ccb0be697bae04f3276
MrSedan <elpashevs@bk.ru>
2023-05-20 00:34:27 +0300

    Added new methods in app

--- backend/app.ts
+++ backend/app.ts
@@ -1,24 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import multer from 'multer';
-import * as crypto from 'crypto';
-import { Prisma, PrismaClient } from '@prisma/client';
-import user from './views/user';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import multer from "multer";
+import user from "./views/user";
+import category from "./views/category";
+import task from "./views/task";
+import morgan from "morgan";
+import cors from "cors";
 
 const storage: multer.StorageEngine = multer.diskStorage({
-   destination: (_req, _file, _cb) => {
-    _cb(null, 'uploads');
-   },
-   filename: (_req, _file, _cb) => {
-    _cb(null, Date.now()+'-'+_file.originalname);
-   }
+    destination: (_req, _file, _cb) => {
+        _cb(null, "uploads");
+    },
+    filename: (_req, _file, _cb) => {
+        _cb(null, Date.now() + "-" + _file.originalname);
+    },
 });
 
 const app: express.Application = express();
-const upload: multer.Multer = multer({storage: storage});
-const prisma = new PrismaClient()
+const upload: multer.Multer = multer({ storage: storage });
 
 const port: number = 3500;
 
@@ -26,17 +25,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors());
-app.use(morgan('dev'));
-app.use('/user',user);
+app.use(morgan("dev"));
+app.use("/user", user);
+app.use("/category", category);
+app.use("/task", task);
 
+app.set("Access-Control-Allow-Origin", "*");
 
-app.set('Access-Control-Allow-Origin', '*');
-
-app.get('/', (_req, _res) => {
-    _res.status(200).send({"status": "ok"});
+app.get("/", (_req, _res) => {
+    _res.status(200).send({ status: "ok" });
 });
 
-app.post('/profile', upload.single('avatar'), (_req, _res, _next) => {
+app.post("/profile", upload.single("avatar"), (_req, _res, _next) => {
     console.log(_req.file, _req.body);
     _res.json(_req.body);
 });
