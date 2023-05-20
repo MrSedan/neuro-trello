9cd1c8d791c892a1c609e119697a9cefb2b00506
burdukow <brdkv19@gmail.com>
2023-05-21 02:24:33 +0500

    Html +CSS for loginPage

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -2,18 +2,18 @@ import { useNavigate } from "react-router-dom";
 import "../assets/home.css";
 
 function HomePage() {
-    const navigate = useNavigate();
-    return (
-        <div
-            className="homePage"
-            onClick={() => {
-                navigate("/test");
-            }}
-        >
-            <h1>Welcome to Neuro-Trello</h1>
-            <h3>Click to go to user login and create</h3>
-        </div>
-    );
+  const navigate = useNavigate();
+  return (
+    <div
+      className="homePage"
+      onClick={() => {
+        navigate("/test");
+      }}
+    >
+      <h1>Welcome to Neuro-Trello</h1>
+      <h3>Click to go to user login and create</h3>
+    </div>
+  );
 }
 
 export default HomePage;
--- frontend/src/views/user/LoginUser.tsx
+++ frontend/src/views/user/LoginUser.tsx
@@ -1,34 +1,39 @@
 import { AxiosError } from "axios";
 import axios from "../../tools/api";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import "../../assets/login.css";
 export default function LoginUserPage() {
-    const navigate = useNavigate();
-    const inputRef = useRef<HTMLInputElement>(null);
-    async function login() {
-        const password = inputRef.current?.value || "";
-        console.log(process.env.REACT_APP_BASE_URL);
-
-        try {
-            await axios.post("/user/login", {
-                password: password,
-            });
-            localStorage.setItem("Password", password);
-            console.log("Saved pass");
-            navigate("/test");
-        } catch (error) {
-            if ((error as AxiosError).response)
-                console.log((error as AxiosError).response!.status);
-            else console.error("Error", error);
-            localStorage.removeItem("Password");
-            console.log("Deleted pass");
-        }
+  const navigate = useNavigate();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState("");
+  async function login() {
+    const password = inputRef.current?.value || "";
+    console.log(process.env.REACT_APP_BASE_URL);
+    try {
+      await axios.post("/user/login", {
+        password: password,
+      });
+      localStorage.setItem("Password", password);
+      console.log("Saved pass");
+      navigate("/test");
+    } catch (error) {
+      if ((error as AxiosError).response)
+        console.log((error as AxiosError).response!.status);
+      else console.error("Error", error);
+      localStorage.removeItem("Password");
+      console.log("Deleted pass");
+      setError("Error!");
     }
-    return (
-        <div>
-            <input type="password" ref={inputRef} />
-            <button onClick={login}>Login</button>
-        </div>
-    );
+  }
+  return (
+    <div className="login">
+      {error !== "" && <div className="error">{error}</div>}
+      <div className="entryField">
+        <h1>Login</h1>
+        <input type="password" ref={inputRef} />
+        <button onClick={login}>Login</button>
+      </div>
+    </div>
+  );
 }
