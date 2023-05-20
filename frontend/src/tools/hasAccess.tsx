4b7d6af9de514ee995cf032fe2f9389322674955
MrSedan <elpashevs@bk.ru>
2023-05-20 21:15:16 +0300

    Protected route middleware

---
+++ frontend/src/tools/hasAccess.tsx
@@ -0,0 +1,31 @@
+import axios from "../tools/api";
+import { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function ProtectedRoutes() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isChecking, setIsChecking] = useState(true);
+    useEffect(() => {
+        const pass = localStorage.getItem("Password") || "";
+        const isAuth = async () => {
+            await axios
+                .post("/user/login", {
+                    password: pass,
+                })
+                .then((res) => {
+                    console.log(res);
+                    setIsLoggedIn(true);
+                    setIsChecking(false);
+                    return;
+                })
+                .catch((e) => {
+                    console.log("You haven't access to this page");
+                    setIsChecking(false);
+                });
+        };
+        isAuth();
+    }, [isLoggedIn]);
+
+    if (isChecking) return <p>Checking...</p>;
+    return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
+}
--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 import HomePage from "./Home";
 import PageNotFound from "./404";
-import CreateUserPage from "./user/CreateUser";
-import GetUserPage from "./user/GetUser";
+import TestPage from "./Test";
+// import CreateUserPage from "./user/CreateUser";
+// import GetUserPage from "./user/GetUser";
 import "../assets/main.css";
+import LoginUserPage from "./user/LoginUser";
+import ProtectedRoutes from "../tools/hasAccess";
 
 function App() {
     return (
@@ -14,8 +17,12 @@ function App() {
                 <Routes>
                     <Route index element={<HomePage />} />
                     <Route path="404" element={<PageNotFound />} />
-                    <Route path="user/create" element={<CreateUserPage />} />
-                    <Route path="user/get/:id" element={<GetUserPage />} />
+                    <Route path="login" element={<LoginUserPage />} />
+                    <Route element={<ProtectedRoutes />}>
+                        <Route path="test" element={<TestPage />} />
+                    </Route>
+                    {/* <Route path="user/create" element={<CreateUserPage />} />
+                        <Route path="user/get/:id" element={<GetUserPage />} /> */}
                     <Route path="*" element={<Navigate to="/404" />} />
                 </Routes>
             </BrowserRouter>
