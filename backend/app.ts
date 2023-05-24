62c09ee350164a9db7e5724e2b270e3611bfb515
Sergey Elpashev <elpashevs@bk.ru>
2023-05-24 22:39:57 +0300

    Global middleware

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import bodyParser from "body-parser";
-import multer from "multer";
+import { json, urlencoded } from "body-parser";
+// import multer from "multer";
 import user from "./views/user";
 import category from "./views/category";
 import task from "./views/task";
@@ -9,20 +9,21 @@ import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
 import { PrismaClient } from "@prisma/client";
+import { userMiddleware } from "./views/tools/middleware";
 
-const storage: multer.StorageEngine = multer.diskStorage({
-    destination: (_req, _file, _cb) => {
-        _cb(null, "uploads");
-    },
-    filename: (_req, _file, _cb) => {
-        _cb(null, Date.now() + "-" + _file.originalname);
-    },
-});
+// const storage: multer.StorageEngine = multer.diskStorage({
+//     destination: (_req, _file, _cb) => {
+//         _cb(null, "uploads");
+//     },
+//     filename: (_req, _file, _cb) => {
+//         _cb(null, Date.now() + "-" + _file.originalname);
+//     },
+// });
 
 const prisma = new PrismaClient();
 
 const app: express.Application = express();
-const upload: multer.Multer = multer({ storage: storage });
+// const upload: multer.Multer = multer({ storage: storage });
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -34,25 +35,26 @@ const io = new Server(server, {
 
 const port: number = 3500;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors());
 app.use(morgan("dev"));
-app.use("/user", user(io));
-app.use("/category", category);
-app.use("/task", task(io));
 
 app.set("Access-Control-Allow-Origin", "*");
 
 app.get("/", (_req, _res) => {
     _res.status(200).send({ status: "ok" });
 });
+app.use("/user", user);
+app.use(userMiddleware);
+app.use("/category", category(io));
+app.use("/task", task(io));
 
-app.post("/profile", upload.single("avatar"), (_req, _res, _next) => {
-    console.log(_req.file, _req.body);
-    _res.json(_req.body);
-});
+// app.post("/profile", upload.single("avatar"), (_req, _res, _next) => {
+//     console.log(_req.file, _req.body);
+//     _res.json(_req.body);
+// });
 
 interface MyError extends Error {
     data: any;
@@ -71,17 +73,9 @@ io.use(async (socket, next) => {
 
 io.on("connection", (socket) => {
     console.log("user connected");
-
     socket.on("disconnect", () => {
         console.log("user disconnected");
         socket.removeAllListeners();
-    });
-    socket.on("message", (message) => {
-        console.log("Client sent message:", message);
-        io.emit("message", "hey, you sent " + message);
-    });
-    socket.on("userConnected", (message) => {
-        console.log(message);
     });
     socket.on("get_tasks", async () => {
         const tasks = await prisma.task.findMany();
--- backend/views/tools/middleware.ts
+++ backend/views/tools/middleware.ts
@@ -5,14 +5,9 @@ import { config as dotenv_config } from "dotenv";
 dotenv_config();
 dotenv_config({ path: ".env.local", override: true });
 
-export const userMiddleware: RequestHandler = (
-    _req: Request,
-    _res: Response,
-    next: NextFunction
-) => {
+export const userMiddleware: RequestHandler = (_req: Request, _res: Response, next: NextFunction) => {
     const authHandler = _req.headers.authorization;
-    if (!authHandler)
-        return _res.status(401).json({ error: "pass is missing" });
+    if (!authHandler) return _res.status(401).json({ error: "pass is missing" });
     if (authHandler === process.env.PASSWORD) return next();
     _res.status(401).json({ error: "wrong password" });
 };
